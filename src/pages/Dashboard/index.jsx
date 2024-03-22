c30b8388ad1254372189ed5a149bf3e84f37c3de
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2024-03-22 12:35:03 +0530

    add vercel.json for deployment

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+import useWorkflowStore from "../../store/workflows";
+import styles from "./styles/index.module.css";
+
 export default function Dashboard() {
-  return <div>
-    
-  </div>;
+  const workflows = useWorkflowStore((state) => state.workflows);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(e.target[0].value);
+    // console.log(e.target);
+  };
+
+  return (
+    <div>
+      <Link to="/" className={styles.link}>
+        Home
+      </Link>
+      <Link to="/dashboard" className={styles.link}>
+        Dashboard
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <input name="file" type="file" />
+        <select name="flow" id="">
+          {workflows.map((workflow) => (
+            <option key={workflow.id} value={workflow}>
+              {workflow.name}
+            </option>
+          ))}
+        </select>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
